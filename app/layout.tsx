bb4ecc23ad1d81b95bc03ebd657bc5cae90866c9
connorforsythco <62790515+connorwforsyth@users.noreply.github.com>
2024-01-23 18:42:55 +1100

    Adding opengraph and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,33 @@ import alphabeta from "@/styles/alphabetaFont";
 
 const inter = Inter({ subsets: ["latin"] });
 import Footer from "@/components/Footer";
+
 export const metadata: Metadata = {
   title: "Makeit — By Designit",
-  description: "An experiment created by Designit Australia",
+  description:
+    "Your team, your users and our squad, creating a prototype that can be tested with your customers right away.",
+  openGraph: {
+    title: "Makeit",
+    description:
+      "Our team, your users and our squad, creating a prototype that can be tested with your customers right away.",
+    url: "https://makeit.designit.com",
+    siteName: "Makeit — by Designit",
+    images: [
+      {
+        url: "/opengraph-image.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "/opengraph-image.png", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
