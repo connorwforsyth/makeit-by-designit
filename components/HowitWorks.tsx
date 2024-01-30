ac37315459275afed07ad7d504dbaef77bcd0ee2
connorforsythco <62790515+connorwforsyth@users.noreply.github.com>
2024-01-30 17:22:19 +1100

    Installing PostHog Tracking
    Using providers.js and .env

--- components/HowitWorks.tsx
+++ components/HowitWorks.tsx
@@ -10,7 +10,7 @@ export function HowItWorksTable({ children }: { children: React.ReactNode }) {
 
 export function HowItWorksItem({ no, title, description }: TableProps) {
   return (
-    <li className="grid grid-cols-1 gap-8 border-t py-12 md:grid-cols-12 md:p-8">
+    <li className=" grid grid-cols-1 gap-8 border-t py-12 md:grid-cols-12 md:p-8">
       <div className="col-span-1 text-2xl">{no}</div>
       <div className="col-span-1 text-4xl md:col-span-5">
         <h3>{title}</h3>
