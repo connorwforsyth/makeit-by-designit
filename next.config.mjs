d246662c1673f7d03f8b54e27c1bd0f2f2e729ae
connorforsythco <62790515+connorwforsyth@users.noreply.github.com>
2024-02-07 10:35:52 +1100

    Adding security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/.*",
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          { key: "Content-Security-Policy", value: "self" },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
